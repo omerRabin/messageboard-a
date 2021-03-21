c6fb31c79926fe4caed1fb3289a9ab86669e1e60
omerRabin <73130830+omerRabin@users.noreply.github.com>
2021-03-21 18:01:48 +0200

    Create Board.cpp

---
+++ Board.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Board.hpp"
+/*
+    Board.cpp.
+    Author: Tal Schreiber.
+*/
+using namespace std;
+
+namespace ariel {
+
+    void Board::post(uint row, uint col, Direction direction, string message) {
+        
+    }
+
+    string Board::read(uint row, uint col, Direction direction, uint length) 
+    {
+        return "";
+    }
+
+    void Board::show() 
+    { 
+    }
+}
